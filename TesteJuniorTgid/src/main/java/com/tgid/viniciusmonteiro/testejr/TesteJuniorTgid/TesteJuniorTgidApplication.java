b34528e41dbcea5f6380d815db7e9c1738b70879
Monteiro77 <monteiro240905@gmail.com>
2024-02-05 10:01:59 -0300

    salvando projeto

---
+++ TesteJuniorTgid/src/main/java/com/tgid/viniciusmonteiro/testejr/TesteJuniorTgid/TesteJuniorTgidApplication.java
@@ -0,0 +1,13 @@
+package com.tgid.viniciusmonteiro.testejr.TesteJuniorTgid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJuniorTgidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteJuniorTgidApplication.class, args);
+	}
+
+}
---
+++ TesteJuniorTgid/src/test/java/com/tgid/viniciusmonteiro/testejr/TesteJuniorTgid/TesteJuniorTgidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgid.viniciusmonteiro.testejr.TesteJuniorTgid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteJuniorTgidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
